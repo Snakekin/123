a3920d4a354d46bddaf21cfc8c8abc611c4965bf
Snakekin <95061445+Snakekin@users.noreply.github.com>
2023-05-27 13:52:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(400,400);
@@ -12,6 +15,11 @@ function setup(){
 
 function draw(){
     background("#5196e3");
+    fill('#F90093');
+   text('Amiya',30,200) ;
+   textSize(difference);
+   document.getElementById("font_size").innerHTML = "The Size of the text  will be " + difference+"px";
+
 }
 
 function modelDone(){
@@ -25,7 +33,11 @@ function gotposes(results,error){
     if(results.length > 0){
         console.log(results);
 
-        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
-        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference =  floor(leftWristX - rightWristX);
+
+        console.log("leftWristX =" + leftWristX + "rightWristX =" + rightWristX + "difference = " + difference);
+    
     }
 }
